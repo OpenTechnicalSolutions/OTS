d8c38e7b0ad9c3789c1663fb5aa6d5f187804176
Allen Roberts <allen.roberts1985@gmail.com>
2018-09-03 00:15:09 -0400

    learning

---
+++ OTS/OTS/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OTS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
