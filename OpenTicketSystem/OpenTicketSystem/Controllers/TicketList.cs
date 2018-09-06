7fb51a3900914e754d9034d0f79e1f9e388d45f1
Allen Roberts <allen.roberts1985@gmail.com>
2018-09-05 22:44:56 -0400

    Added ticket queue

---
+++ OpenTicketSystem/OpenTicketSystem/Controllers/TicketList.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OpenTicketSystem.Models;
+using OpenTicketSystem.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpenTicketSystem.Controllers
+{
+    public class TicketList : Controller
+    {
+        private readonly ITicketRepository _ticketRepository;
+
+        public TicketList(ITicketRepository ticketRepository)
+        {
+            _ticketRepository = ticketRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var tickets = _ticketRepository.GetTickets();
+            var ticketViewModelList = new List<TicketPreviewViewModel>();
+            foreach (var t in tickets)
+                ticketViewModelList.Add(new TicketPreviewViewModel()
+                {
+                    TicketId = t.Id,
+                    TimeStamp = t.TimeStamp,
+                    Subject = t.Subject
+                });
+            return View(ticketViewModelList);
+        }
+
+        public IActionResult TicketDetails(int id)
+        {
+            var ticket = _ticketRepository.GetTicketById(id);
+            return View(ticket);
+        }
+    }
+}
---
+++ OpenTicketSystem/OpenTicketSystem/ViewModels/TicketPreviewViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpenTicketSystem.ViewModels
+{
+    public class TicketPreviewViewModel
+    {
+        public int TicketId { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string Subject { get; set; }
+    }
+}
