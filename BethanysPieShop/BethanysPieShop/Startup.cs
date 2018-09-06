5573bd88a156dd85a33e37375ccae4266d508f24
Allen Roberts <allen.roberts1985@gmail.com>
2018-09-05 21:23:24 -0400

    Complete with identity

--- BethanysPieShop/BethanysPieShop/Startup.cs
+++ BethanysPieShop/BethanysPieShop/Startup.cs
@@ -6,6 +6,7 @@ using BethanysPieShop.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -28,6 +29,10 @@ namespace BethanysPieShop
             //register framework services
             services.AddDbContext<AppDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>();
+
             services.AddTransient<IPieRepository, PieRepository>();
             services.AddTransient<IFeedbackRepository, FeedbackRespository>();
             services.AddMvc();
@@ -42,7 +47,11 @@ namespace BethanysPieShop
             }
 
             app.UseStatusCodePages();
+            //must come before MVC to increase responsiveness
             app.UseStaticFiles();
+            //Must come before MVC to work.
+            app.UseAuthentication();
+
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
