9c3c03e387d4f58f70d17f53f22da1493894494d
Allen Roberts <allen.roberts1985@gmail.com>
2018-09-17 15:45:21 -0400

    added room controller for ajax, sorted repositories by ticket system part

---
+++ OpenTicketSystem/OpenTicketSystem/Controllers/RoomController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OpenTicketSystem.Models.Locations;
+using OpenTicketSystem.Repositories.LocationRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpenTicketSystem.Controllers
+{
+    public class RoomController : Controller
+    {
+        private RoomRepository _roomRepository;
+
+        public RoomController (RoomRepository roomRepository)
+        {
+            _roomRepository = roomRepository;
+        }
+
+        public JsonResult Index()
+        {
+            return Json(_roomRepository.GetAll());
+        }
+
+        public IActionResult RoomDetails(int id)
+        {
+            return View(_roomRepository.GetById(id));
+        }
+
+        public IActionResult AddRoom()
+        {
+            return View();
+        }
+        [HttpPost]
+        public void AddRoom(Room room)
+        {
+            _roomRepository.Add(room);
+        }
+    }
+}
--- OpenTicketSystem/OpenTicketSystem/Repositories/LocationRepositories/DepartmentRepository.cs
+++ OpenTicketSystem/OpenTicketSystem/Repositories/LocationRepositories/DepartmentRepository.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace OpenTicketSystem.Repositories
+namespace OpenTicketSystem.Repositories.LocationRepositories
 {
     public class DepartmentRepository : IRepository<DepartmentModel>
     {
