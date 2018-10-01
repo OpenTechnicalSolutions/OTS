db12fa8fe5c3baee42d29012879cc621d8225355
Allen Roberts <allen.roberts1985@gmail.com>
2018-09-30 22:11:26 -0400

    Ajax query now working.

---
+++ OpenTicketSystem/OpenTicketSystem/wwwroot/lib/LocationCall.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+    $("#departments").click(function () {
+        console.log("Drawing Departments")
+        queryData('Department');
+    })
+
+    $("#buildings").click(function () {
+        console.log("Drawing Buildings")
+        queryData('Building');
+    });
+});
+
+function queryData(urlData) {
+    $.ajax({
+        url: urlData,
+        type: "GET",
+        cache: false,
+    }).done(function (result) {
+        console.log("Query Complete")
+        $("#partialView").empty();
+        $("#partialView").append(result);
+    });
+}
