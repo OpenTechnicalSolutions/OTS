08b34fbf3af24b7abb8ce383db037f54e3ca5389
Allen Roberts <allen.roberts1985@gmail.com>
2018-10-05 16:40:58 -0400

    modified Account views to work with adapter.

---
+++ OpenTicketSystem/OpenTicketSystem/scripts/AccountPageBehavior.ts
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    var techStatus = $("#techStatus").data("techStat");
+    $.ajax({
+        url: "/Department/DepartmentData",
+        dataType: "text/plain",
+        type: "GET"
+    }).done(
+        function (result) {
+
+        });
+});
